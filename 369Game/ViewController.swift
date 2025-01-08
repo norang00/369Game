c962bbaf5c8f98e22822f0e91c4406c6a100315b
norang <norang1221@gmail.com>
2025-01-08 16:09:53 +0900

    Initial Commit

---
+++ 369Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  369Game
+//
+//  Created by Kyuhee hong on 1/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
