ceaa45de3f5a8ce1ac641cfc4fa1589a240ed384
norang <norang1221@gmail.com>
2025-01-08 19:27:02 +0900

    PickerView 숫자 선택 기능 구현

--- 369Game/ViewController.swift
+++ 369Game/ViewController.swift
@@ -14,17 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet var numbersLabel: UILabel!
     @IBOutlet var clapResultLabel: UILabel!
 
+    var pickerView = UIPickerView()
+    var numberList: [Int] = []
+    var selectedNumber: Int = 0 {
+        didSet {
+            clapResultLabel.text = "숫자 \(selectedNumber)까지 총 박수는 \(clapCount)번 입니다."
+        }
+    }
+    
+    var clapCount: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureView()
-
-
+        makeNumberArray()
+        
+        setTextField()
+        setPickerView()
     }
-
-    
-    // MARK: - View Settings
-    
+        
     func configureView() {
         gameTitleLabel.text = "369 게임"
         gameTitleLabel.font = .systemFont(ofSize: 34, weight: .bold)
@@ -35,22 +44,83 @@ class ViewController: UIViewController {
         inputTextfield.font = .systemFont(ofSize: 24, weight: .medium)
         inputTextfield.textAlignment = .center
         
-        numbersLabel.text = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18"
-        numbersLabel.font = .systemFont(ofSize: 24, weight: .light)
-        numbersLabel.textColor = .lightGray
+        numbersLabel.text = "숫자들"
+        numbersLabel.font = .systemFont(ofSize: 22, weight: .light)
+        numbersLabel.textColor = .gray
         numbersLabel.textAlignment = .center
         numbersLabel.numberOfLines = 0
         numbersLabel.frame.size = numbersLabel.intrinsicContentSize
         
-        clapResultLabel.text = "숫자 99까지 총 박수는 45번 입니다."
+        clapResultLabel.text = "숫자 \(selectedNumber)까지 총 박수는 \(clapCount)번 입니다."
         clapResultLabel.font = .systemFont(ofSize: 32, weight: .bold)
         clapResultLabel.textAlignment = .center
         clapResultLabel.numberOfLines = 0
     }
-
-
     
+    func makeNumberArray() {
+        for index in 1...100 {
+            numberList.append(index)
+        }
+        numberList.reverse()
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+
+    func setTextField() {
+        inputTextfield.delegate = self
+        inputTextfield.inputView = pickerView
+    }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        false
+    }
+
+    @IBAction func tapGestureRecognizerTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        inputTextfield.text = ""
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func setPickerView() {
+        pickerView.delegate = self
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        100
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(numberList[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let item = numberList[row]
+        inputTextfield.text = String(item)
+        selectedNumber = item
+        drawNumbers(item)
+    }
     
 }
 
+extension ViewController {
+    
+    func drawNumbers(_ selectedNumber: Int) {
+        var numbersText: String = ""
+        for index in 1...selectedNumber {
+            numbersText += "\(index), "
+            
+            if index == selectedNumber {
+                numbersText += "\(index)"
+            }
+        }
+        numbersLabel.text = numbersText
+    }
+    
+}
